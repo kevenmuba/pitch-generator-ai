5bc0286140e7b18609c939e6c33d2ce0ae9e64d5
kevenmuba <kevk0758@gmail.com>
2025-12-25 20:32:25 +0300

    adding health test function for deployment

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -8,8 +8,9 @@ import { TransactionsModule } from './transactions/transactions.module';
 import { TemplatesModule } from './templates/templates.module';
 import { UserLessonsModule } from './user-lessons/user-lessons.module';
 import { PitchesModule } from './pitches/pitches.module';
+import { HealthModule } from './health/health.module';
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule, TransactionsModule, TemplatesModule,UserLessonsModule, PitchesModule],
+  imports: [PrismaModule, UsersModule, AuthModule, TransactionsModule, TemplatesModule,UserLessonsModule, PitchesModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
